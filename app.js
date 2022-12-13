d8e3d7ed23e03a1a4b231c17dd679545bf790903
trung110152 <trung110152@gmail.com>
2022-12-13 13:49:19 +0700

    v1

---
+++ app.js
@@ -0,0 +1,23 @@
+async function getSum(arr) {
+    if (arr instanceof Array){
+        let sum=0
+        for (let i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        return   sum ;
+
+    }
+    throw new Error("Input data is incorrect")
+}
+
+async function f() {
+    try{
+        let result = await getSum([3,4,5]);
+        console.log(result);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+f();
+
