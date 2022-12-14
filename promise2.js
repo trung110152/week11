2cfe76ddda511b8548a1ababe08a734b3615af47
trung110152 <trung110152@gmail.com>
2022-12-14 11:21:57 +0700

    3

--- promise2.js
+++ promise2.js
@@ -1,24 +1,40 @@
-let axios = require('axios'); // lấy thư viện vào máy
-
-function getData() {
-    return new Promise((resolve, reject) => {
-            axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then((data) => {
-                    resolve(data)
-                }
-            )
-        }
-    )
-}
+// let axios = require('axios');
+//
+// async function run() {
+//     let list = [];
+//     await axios.get('https://jsonplaceholder.typicode.com/users/1').then((res) => {
+//         list.push(res.data)
+//     })
+//     await axios.get('https://jsonplaceholder.typicode.com/users/2').then((res) => {
+//         list.push(res.data)
+//     })
+//     await axios.get('https://jsonplaceholder.typicode.com/users/3').then((res) => {
+//         list.push(res.data)
+//     })
+//     console.log(list.filter(item => item.name.includes("le")))
+//
+//
+// }
+//
+// run().then();
 // async - await
-async function show() {
-    let data = await getData();
-    console.log(data.data)
-}
-
-
-show();
+// async function show() {
+//     let data = await getData();
+//     console.log(getData())
+// }
+//
+//
+// show();
 
 // promise
 // getData().then((data) => {
 //     console.log(data.data)}
 // )
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.write('<h1> Hello world </h1><hr>');
+    res.end();
+});
+server.listen(8080, 'localhost');
+
